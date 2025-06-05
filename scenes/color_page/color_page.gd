fbd4f6e151c275cee5590a8ef94fce39546db1aa
“[charleslynn1969@gmail.com]” <charleslynn1969@gmail.com>
2025-06-05 17:34:58 -0500

    Working  app
    needs gui and paint stuff added

--- scenes/color_page/color_page.gd
+++ scenes/color_page/color_page.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 #PageToColor script
 var image_path = [
+	"res://pages/0.jpeg",
 	"res://pages/1.jpeg",
 	"res://pages/2.jpeg",
 	"res://pages/3.jpeg",
@@ -10,20 +11,40 @@ var image_path = [
 	"res://pages/7.jpeg",
 	"res://pages/8.jpeg",
 	"res://pages/9.jpeg",
-	"res://pages/10.jpeg",
 	
 ]
 var max_images = 9
 var count = 0
+var brush_size = 8
+var brush_color := Color(1, 0, 0)  # Red
+func _ready() -> void:
+	# LOad the first page
+	$ColorRect/PageSprite2D.texture = load(image_path[count])
+	
 
 
 func _on_button_pressed() -> void:
-	print("Count Top : ",count)
-	if count < max_images:
-		print("Image path: ",image_path[count])
+	count += 1
+	if count <= max_images:
 		$ColorRect/PageSprite2D.texture = load(image_path[count])
+		
 	else:
 		$Button.text = " Finished!"
-	count += 1
-	print("Count: ",count)
+
+		
 	
+		
+	
+	
+	
+	
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		_paint(event.position)
+	elif event is InputEventMouseMotion and event.button_mask & MOUSE_BUTTON_MASK_LEFT:
+			_paint(event.position)
+			
+func _paint(pos: Vector2):
+	var px = int(pos.x)
+	var py = int(pos.y)
+	print("Paint funcion To-Do"," X-> ",px," Y->  ",py)
